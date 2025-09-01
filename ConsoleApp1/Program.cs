7488784b8df8c58f35ee73a70b7ad7e7bdac04d7
lassemanicus <lasse.manicus@gmail.com>
2025-09-01 16:10:26 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ConveyorBeltCapacityCheck
+{
+    static void Main()
+    {
+        Console.Write("How many motors are carrying the packages? ");
+        int motors = int.Parse(Console.ReadLine());
+
+        Console.Write("How many kg of packages do we expect? ");
+        double weight = double.Parse(Console.ReadLine());
+        
+        double loadPerMotor = weight / motors;
+        
+        if (loadPerMotor <= 5.6)
+        {
+            Console.WriteLine("Yes! The conveyor belt can carry the packages.");
+        }
+        else
+        {
+            Console.WriteLine("No. The conveyor belt cannot carry the packages.");
+        }
+    }
+}
